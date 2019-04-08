0baa72cba51e140c326b957326b739bf0caba306
Kenny Hong <k.hongjiahan@gmail.com>
2019-04-08 16:36:19 -0500

    adding this to be worked on at home

---
+++ Assets/Scripts/AppManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Phidget22;
+using PhidgetsControlLibrary;
+
+
+public class AppManager : MonoBehaviour {
+
+    VoltageRatioInput ratio = null;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
